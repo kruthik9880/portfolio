7d39396cf5eeda5ef1ab30754813598be2a8da52
Kruthik Gandhi H A <83821642+kruthik9880@users.noreply.github.com>
2022-09-08 15:01:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Software Engineer", "Traveller", "Fast Learner"],
+        strings: ["Software Engineer", "Android developer", " wanderer", "connoisseur of food"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "Software Engineer", "Traveller", "Fast Learner"],
+        strings:  ["Software Engineer", "Android developer", " wanderer", "connoisseur of food"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
